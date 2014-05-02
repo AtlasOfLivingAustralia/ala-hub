017b068c240635c906a5c2949e04adf65c56804c
nickdos <nickdos@gmail.com>
2014-05-02 05:45:51 +0000

    Added more debugging to bootstrap.groovy.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/ala-hub@1408 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,9 +3,10 @@ class BootStrap {
     def grailsApplication
 
     def init = { servletContext ->
-        log.info "config.security.cas.bypass = ${grailsApplication.config.security.cas.bypass}"
+        log.info "config.security.cas = ${grailsApplication.config.security.cas}"
         log.info "config.ala.skin = ${grailsApplication.config.ala.skin}"
         log.info "config.biocache.ajax.useProxy = ${grailsApplication.config.biocache.ajax.useProxy}"
+        log.info "config.grails.serverURL = ${grailsApplication.config.grails.serverURL}"
     }
     def destroy = {
     }
