80689d4d710a71a8ca4f50435ce454cb9a311b38
nickdos <nickdos@gmail.com>
2014-05-12 03:12:41 +0000

    Attempted fix for bootstrap response CSS being absent on prod server.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/ala-hub@1428 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -6,7 +6,15 @@ modules = {
     // Define your skin module here - it must 'dependsOn' either bootstrap (ALA version) or bootstrap2 (unmodified) and core
 
     ala {
-        dependsOn 'bootstrap', 'hubCore' // from ala-web-theme plugin
+        dependsOn 'bootstrapLocal', 'hubCore' // from ala-web-theme plugin
+    }
+
+    bootstrapLocal {
+        dependsOn 'core'
+        resource url:[dir:'js', file:'bootstrap.js', plugin: 'ala-web-theme', disposition: 'head']
+        resource url:[dir:'css', file:'bootstrap.css', plugin: 'ala-web-theme'], attrs:[media:'screen, projection, print']
+        resource url:[dir:'css', file:'bootstrap-responsive.css', plugin: 'ala-web-theme'], attrs:[media:'screen'] // id:'responsiveCss'
+        resource url:[dir:'css', file:'ala-extra.css'], attrs:[media:'screen']
     }
 
 }
