a4723d4798d3c1122e603fa9a4c8ed21ebeab524
nickdos <nickdos@gmail.com>
2014-09-01 14:18:53 +1000

    Excluded bootstrap.css from resource processing

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -13,7 +13,7 @@ modules = {
         dependsOn 'core'
         defaultBundle 'main-core'
         resource url:[dir:'js', file:'bootstrap.js', plugin: 'ala-web-theme'], disposition: 'head', exclude: '*'
-        resource url:[dir:'css', file:'bootstrap.css', plugin: 'ala-web-theme'] //, attrs:[media:'screen, projection, print']
+        resource url:[dir:'css', file:'bootstrap.css', plugin: 'ala-web-theme'], exclude: '*' //, attrs:[media:'screen, projection, print']
         resource url:[dir:'css', file:'bootstrap-responsive.css', plugin: 'ala-web-theme'], exclude: '*', attrs:[media:'screen'] // id:'responsiveCss'
         resource url:[dir:'css', file:'ala-extra.css']//, attrs:[media:'screen']
     }
