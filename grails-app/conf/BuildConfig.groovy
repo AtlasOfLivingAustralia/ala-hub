a97f287d2ab5dcf43eb522c4afd24197d3854cf1
Dave Martin <djtfmartin@gmail.com>
2014-10-31 13:52:31 +1100

    point at nexus.ala.org.au

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,6 +45,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://nexus.ala.org.au/content/repositories/releases/"
         mavenRepo ("http://maven.ala.org.au/repository") {
             updatePolicy 'always'
         }
