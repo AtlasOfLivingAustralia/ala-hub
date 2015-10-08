7faed20478cc8f5469703443f0074ff8d8c0ea7e
Dave Martin <djtfmartin@gmail.com>
2015-10-08 14:33:42 +0100

    skin fixes to avoid clobbering of "ala" resource module

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -5,7 +5,7 @@ modules = {
 
     // Define your skin module here - it must 'dependsOn' either bootstrap (ALA version) or bootstrap2 (unmodified) and core
 
-    ala {
+    ala_extras {
         dependsOn 'bootstrap', 'hubCore' // from ala-web-theme plugin
         defaultBundle 'main-core'
         resource url:[dir:'css', file:'ala-extra.css']
