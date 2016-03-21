600f4486a8077ad86084691fb5498216b13bd574
Nick dos Remedios <nickdos@gmail.com>
2016-03-21 16:08:44 +1100

    Update downloads-plugin to 1.1-SANPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -55,7 +55,7 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
         runtime ":biocache-hubs:0.75"
-        runtime ":downloads-plugin:1.0"
+        runtime ":downloads-plugin:1.1-SNAPSHOT"
         runtime ":ala-bootstrap2:2.4"
         runtime ":ala-auth:1.3.1"
     }
