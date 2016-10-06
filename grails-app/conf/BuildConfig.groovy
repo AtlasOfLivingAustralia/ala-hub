81ba4abb203acf090bc92e59518bf33934fd4c24
nickdos <nickdos@gmail.com>
2016-10-07 08:17:08 +1100

    Updated plugin versions

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -57,8 +57,8 @@ grails.project.dependency.resolution = {
         compile ":cache-headers:1.1.6"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:1.2-SNAPSHOT"
-        runtime ":downloads-plugin:1.1"
+        runtime ":biocache-hubs:1.1.2"
+        runtime ":downloads-plugin:1.2.1"
         runtime ":ala-bootstrap2:2.4.3-SNAPSHOT"
         runtime ":ala-auth:1.3.4"
     }
