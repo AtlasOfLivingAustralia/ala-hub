eaf497649d07527f78ed7045cb24660f5eacc960
adam-collins <adam.collins832@gmail.com>
2016-10-25 09:40:06 +1000

    Update biocache-hubs plugin to 1.3-SNAPSHOT

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -57,7 +57,7 @@ grails.project.dependency.resolution = {
         compile ":cache-headers:1.1.6"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:1.1.2"
+        runtime ":biocache-hubs:1.3-SNAPSHOT"
         runtime ":downloads-plugin:1.2.2"
         runtime ":ala-bootstrap2:2.4.3-SNAPSHOT"
         runtime ":ala-auth:1.3.4"
