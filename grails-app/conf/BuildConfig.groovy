4ff2df2ae372646c874083cb0cb961eba7776cd3
nickdos <nickdos@gmail.com>
2016-11-29 09:12:50 +1100

    Moved GeoLiteCity into ala-hub
    Updated biocache-hubs to 1.3.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -55,9 +55,10 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.54"
         compile ':cache:1.1.1'
         compile ":cache-headers:1.1.6"
+        compile ":geoip:0.3.3"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:1.3.4"
+        runtime ":biocache-hubs:1.3.5"
         runtime ":downloads-plugin:1.2.2"
         runtime ":ala-bootstrap2:2.4.4"
         runtime ":ala-auth:1.3.4"
