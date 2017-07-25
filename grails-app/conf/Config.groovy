4639b55f62fac2b68eb01acf964d1a8bc480a19d
nickdos <nickdos@gmail.com>
2017-07-25 15:47:55 +1000

    URL for EYA breadcrumb

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -39,6 +39,7 @@ skin.taxaLinks.baseUrl = "http://bie.ala.org.au/species/"
 skin.headerUrl = "classpath:resources/generic-header.jsp" // can be external URL
 skin.footerUrl = "classpath:resources/generic-footer.jsp" // can be external URL
 skin.fluidLayout = true // true or false
+skin.exploreUrl = "https://www.ala.org.au/explore-by-location/"
 
 /******************************************************************************\
  *  EXTERNAL SERVERS
