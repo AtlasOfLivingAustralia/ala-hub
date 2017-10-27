6c63889de257733273e4fafc3d6744b7759d1486
nickdos <nickdos@gmail.com>
2017-10-27 16:58:05 +1100

    Update biocache-hubs:1.4.6 and downloads-plugin:1.3.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -58,8 +58,8 @@ grails.project.dependency.resolution = {
         compile ":geoip:0.3.3"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:1.4.4"
-        runtime ":downloads-plugin:1.3.2"
+        runtime ":biocache-hubs:1.4.6"
+        runtime ":downloads-plugin:1.3.3"
         runtime ":ala-bootstrap2:2.5-SNAPSHOT"
         runtime ":ala-auth:1.3.4"
     }
