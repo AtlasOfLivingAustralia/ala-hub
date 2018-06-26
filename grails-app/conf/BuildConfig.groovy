0e8fbb7fcce77802577f42f5124eb1d0cc1c973e
nickdos <nickdos@gmail.com>
2018-06-26 14:57:41 +1000

    Hotfix 2.2.9
    Biocache-hubs upgrade to 1.4.10

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -58,7 +58,7 @@ grails.project.dependency.resolution = {
         compile ":geoip:0.3.3"
         runtime ":resources:1.2.8"
         runtime ":cached-resources:1.0"
-        runtime ":biocache-hubs:1.4.9"
+        runtime ":biocache-hubs:1.4.10"
         runtime ":downloads-plugin:1.3.3"
         runtime ":ala-bootstrap2:2.5-SNAPSHOT"
         runtime ":ala-auth:1.3.4"
